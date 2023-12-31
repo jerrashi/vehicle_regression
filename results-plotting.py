4a90889a6ab7d2be0a7a3e2281fca91337f02e56
Jerry Shi <jerryshi25@gmail.com>
2023-12-30 21:38:03 -0500

    finished plotting

--- results-plotting.py
+++ results-plotting.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 import numpy as np
 import regex as re
+import plotly.graph_objs as go
 
 # Step 1: Parse the HTML File
 with open("results.html", "r") as file:
@@ -54,12 +55,11 @@ for div in soup.find_all("div", class_="srp-listing-content"):
         # Locate the Accident Tag and Extract Accident
         accident_tag = article.find("span", class_="title title--noAccident")
         if accident_tag:
+            has_accident = False
+            has_damage = False
             condition = accident_tag.get_text(strip=True)
             condition_lower = condition.lower()
-            if "no" in condition_lower:
-                has_accident = False
-                has_damage = False
-            else:
+            if "no " not in condition_lower:
                 if "accident" in condition_lower:
                     has_accident = True
                 if "damage" in condition_lower:
@@ -138,13 +138,6 @@ def prepare_data(data):
 X_with_accidents, y_with_accidents = prepare_data(listings_with_accidents)
 X_without_accidents, y_without_accidents = prepare_data(listings_without_accidents)
 
-'''
-print("X with accidents:", X_with_accidents)
-print("y with accidents:", y_with_accidents)
-print("X without accidents:", X_without_accidents)
-print("y without accidents:", y_without_accidents)
-'''
-
 # Perform linear regression
 model_with_accidents = LinearRegression().fit(X_with_accidents, y_with_accidents)
 model_without_accidents = LinearRegression().fit(X_without_accidents, y_without_accidents)
@@ -156,6 +149,41 @@ coef_without_accidents = model_without_accidents.coef_[0]
 print("Coefficient for cars with accidents:", coef_with_accidents)
 print("Coefficient for cars without accidents:", coef_without_accidents)
 
+# Get the intercepts
+intercept_with_accidents = model_with_accidents.intercept_
+intercept_without_accidents = model_without_accidents.intercept_
 
-# Step 4: Plot the data using plotly to generate an interactive scatterplot 
-# visualization including line of best fit
+print("Intercept for cars with accidents:", intercept_with_accidents)
+print("Intercept for cars without accidents:", intercept_without_accidents)
+
+# Step 4: Plot the data using plotly to generate an interactive scatterplot visualization
+
+def create_scatter_trace(listings, name, color):
+    x = [listing['Mileage'] for listing in listings]
+    y = [listing['Price'] for listing in listings]
+    text = [f"<img src='{listing['Image_URL']}' width='100px'><br>{listing['Title']}<br>Mileage: {listing['Mileage']}<br>Condition: {listing['Condition']}<br>VIN: {listing['VIN']}<br>URL: {listing['URL']}" for listing in listings]
+    return go.Scatter(x=x, y=y, mode='markers', name=name, marker_color=color, text=text, hoverinfo='text')
+
+# Create scatter traces
+trace_with_accidents = create_scatter_trace(listings_with_accidents, 'With Accidents', 'red')
+trace_without_accidents = create_scatter_trace(listings_without_accidents, 'Without Accidents', 'blue')
+
+# Get the overall range of mileage
+all_mileage = [listing['Mileage'] for listing in listings_with_accidents + listings_without_accidents]
+min_mileage = min(all_mileage)
+max_mileage = max(all_mileage)
+x_range = np.linspace(min_mileage, max_mileage, 100)
+
+# Create lines of best fit
+line_with_accidents = go.Scatter(x=x_range, y=model_with_accidents.intercept_ + model_with_accidents.coef_[0] * x_range, mode='lines', name='Best Fit With Accidents', line=dict(color='red', dash='dash'))
+line_without_accidents = go.Scatter(x=x_range, y=model_without_accidents.intercept_ + model_without_accidents.coef_[0] * x_range, mode='lines', name='Best Fit Without Accidents', line=dict(color='blue', dash='dash'))
+
+# Combine traces
+data = [trace_with_accidents, trace_without_accidents, line_with_accidents, line_without_accidents]
+
+# Create layout
+layout = go.Layout(title='Car Listings: Mileage vs Price', xaxis=dict(title='Mileage'), yaxis=dict(title='Price'), hovermode='closest')
+
+# Create figure and plot
+fig = go.Figure(data=data, layout=layout)
+fig.show()
